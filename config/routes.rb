2580bc98996c9eef24e2a28b48d9172455fe5b81
Al Ilseman <al@ilseman.com>
2016-02-14 19:05:38 +0000

    namespaced api route and ignored temp and log

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :tracks, except: [:new, :edit]
+
+  scope '/api' do
+    resources :tracks, except: [:new, :edit]
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
