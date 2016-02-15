3dc4b507a5a5427fc27d9739047be8dd01b8ef42
Al Ilseman <al@ilseman.com>
2016-02-15 02:30:05 +0000

    stubbed track validation specs

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Track, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a path, artist_id, album_id, title, track_number, quality_id, and user_id"
+  it "is invlaid without a path"
 end
