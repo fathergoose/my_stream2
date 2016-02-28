8e1a43fbaf79865744e8b0a40e4e0c2d8c49a580
Al Ilseman <al@ilseman.com>
2016-02-27 21:18:15 -0600

    copy pasted ng-uploader example--works

--- myTunes/app/scripts/controllers/main.js
+++ myTunes/app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the myTunesApp
  */
 angular.module('myTunesApp')
-  .controller('MainCtrl', function (audio, player, $scope) {
+  .controller('MainCtrl', function (audio, player, $scope, $log, uiUploader) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -18,4 +18,34 @@ angular.module('myTunesApp')
     $scope.message = "hello";
     $scope.audio = audio;
     $scope.player = player;
+
+    $scope.btn_remove = function(file) {
+      $log.info('deleting=' + file);
+      uiUploader.removeFile(file);
+    };
+    $scope.btn_clean = function() {
+      uiUploader.removeAll();
+    };
+    $scope.btn_upload = function() {
+      $log.info('uploading...');
+      uiUploader.startUpload({
+        url: '/api/tracks',
+        concurrency: 2,
+        onProgress: function(file) {
+          $log.info(file.name + '=' + file.humanSize);
+          $scope.$apply();
+        },
+        onCompleted: function(file, response) {
+          $log.info(file + 'response' + response);
+        }
+      });
+    };
+    $scope.files = [];
+    var element = document.getElementById('file1');
+    element.addEventListener('change', function(e) {
+      var files = e.target.files;
+      uiUploader.addFiles(files);
+      $scope.files = uiUploader.getFiles();
+      $scope.$apply();
+    });
   });
