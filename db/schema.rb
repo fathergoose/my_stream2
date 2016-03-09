3269ddef2dc185c4a7a8ea57a8ba7ab1c5cf3c7c
fathergoose <al@ilseman.com>
2016-03-09 16:04:33 -0600

    added and migrated artist id to tracks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309215114) do
+ActiveRecord::Schema.define(version: 20160309220009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160309215114) do
     t.string   "artist"
     t.string   "album"
     t.integer  "album_id"
+    t.integer  "artist_id"
   end
 
 end
