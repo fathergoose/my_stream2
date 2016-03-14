41fdbbb34a0ef9481bcc5346d62398f7d18ddd4e
Dennise Saxton <dennisepnd@gmail.com>
2016-03-14 18:48:23 -0500

    Added artist and album tests, changed spelling error in track spec file

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Album, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a title"
+  it "is invalid without a title"
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a name"
+  it "is invalid without a name"
 end
